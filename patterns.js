32a6c8db3061e716142ac344e3c8effb5f6a2587
dom-o <m.domonic+github@gmail.com>
2017-11-23 04:05:30 -0500

    Initial commit; beginnings of vocab list/sentence patterns.

---
+++ patterns.js
@@ -0,0 +1,32 @@
+
+//Beginning quantifiers
+[
+    'wIntensifier climbAdj.',
+    'A climbAdj nClimb'
+]
+
+//Start descriptions
+[
+    'vStart in nFeature',
+    'vStart on nHold',
+    'vStart on nHold in nFeature'
+]
+
+//Middle descriptions
+[
+    'vDo a moveAdj nMove',
+    'vMove on nHolds',
+    'vMove to a nHold',
+    'vHold a holdAdj nHold',
+    'Keep it together for a moveAdj nMove',
+    'Stay sharp for a moveAdj nMove'
+]
+[
+    'into a holdAdj nHold',
+    'past a holdAdj nHold',
+]
+
+//End descriptions.
+[
+    ''
+]
---
+++ vocab.js
@@ -0,0 +1,172 @@
+//wIntensifier
+[
+    'super',
+    'highly',
+    'insanely',
+    'ultra',
+]
+
+//nClimb
+[
+    'crimpfest',
+    'slopefest',
+    'pumpfest',
+    'chosspile',
+    'testpiece',
+    'rig',
+    'bloc',
+    'highball'
+]
+
+//nFeature
+[
+    'scoop',
+    'seam',
+    'hueco',
+    'crack',
+    'cave',
+    'roof',
+    'pit',
+    
+]
+
+//nHold
+[
+    'sloper',
+    'jug',
+    'crimp',
+    'rail',
+    'undercling',
+    'edge',
+    'pinch',
+    'pocket',
+    'mono',
+    'slot'
+]
+
+//nHolds
+[
+    'slopers',
+    'jugs',
+    'crimps',
+    'rails',
+    'underclings',
+    'edges',
+    'pinches',
+    'pockets',
+    'monos',
+    'slots'
+]
+
+//nMove
+[
+    'move',
+    'deadpoint',
+    'dyno',
+    'cross',
+    'high-step',
+    'pull',
+    'lock-off',
+    'reach',
+    'drop-knee'
+]
+
+//vStart
+[
+    'establish',
+    'start',
+]
+
+//vDo
+[
+    'make',
+    'pull',
+    'do',
+]
+
+//vMove
+[
+    'move',
+    'traverse',
+    'thrutch',
+    'huck',
+    'burl',
+    'throw',
+    'pull',
+]
+
+//vHold
+[
+    'crimp',
+    'squeeze',
+    'pinch',
+    'control',
+    'dominate',
+]
+
+//climbAdj
+[
+    'thought-provoking',
+    'interesting',
+    'fun',
+    'flowy',
+    'techy',
+    'technical',
+    'mega',
+    'chossy',
+    'insecure',
+    'morpho',
+    'burly',
+    'height-dependent',
+    'sandbagged',
+    'long',
+    'short',
+    'cruxy',
+    'sustained',
+    'deathly',
+    'gym-style',
+    'beta-intensive'
+]
+
+//moveAdj
+[
+    'delicate',
+    'strong',
+    'insecure',
+    'long',
+    'short',
+    'burly',
+    'technical',
+]
+
+//holdAdj
+[
+    'slopey',
+    'pinchey',
+    'crimpy',
+    'heinous',
+    'terrible',
+    'barely-there',
+    'credit-card',
+    'sharp',
+    'iron',
+    'glassy',
+    'slick',
+    'sticky',
+    'wide',
+    'deep',
+    'thick',
+    'thin',
+    'wavy',
+    'juggy',
+    'bomber',
+    'sinker',
+    'huge',
+    'giant',
+    'frightful',
+    'full-pad',
+    'half-pad',
+    'quarter-pad',
+    'two-finger',
+    'three-finger'
+]
