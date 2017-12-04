02309ba19b5f5f61c0432107a5d3426fae97c431
dom-o <m.domonic+github@gmail.com>
2017-12-03 22:56:53 -0500

    New vocab added.

--- vocab.js
+++ vocab.js
@@ -1,172 +1,166 @@
-//wIntensifier
-[
-    'super',
-    'highly',
-    'insanely',
-    'ultra',
-]
+words = {
+    wIntensifier: [
+        'super',
+        'highly',
+        'insanely',
+        'ultra',
+    ],
 
-//nClimb
-[
-    'crimpfest',
-    'slopefest',
-    'pumpfest',
-    'chosspile',
-    'testpiece',
-    'rig',
-    'bloc',
-    'highball'
-]
+    nClimb: [
+        'crimpfest',
+        'slopefest',
+        'pumpfest',
+        'chosspile',
+        'testpiece',
+        'rig',
+        'bloc',
+        'highball'
+    ],
 
-//nFeature
-[
-    'scoop',
-    'seam',
-    'hueco',
-    'crack',
-    'cave',
-    'roof',
-    'pit',
-    
-]
+    nFeature: [
+        'scoop',
+        'seam',
+        'hueco',
+        'crack',
+        'cave',
+        'roof',
+        'pit',
+    ],
 
-//nHold
-[
-    'sloper',
-    'jug',
-    'crimp',
-    'rail',
-    'undercling',
-    'edge',
-    'pinch',
-    'pocket',
-    'mono',
-    'slot'
-]
+    nHold: [
+        'sloper',
+        'jug',
+        'crimp',
+        'rail',
+        'undercling',
+        'edge',
+        'pinch',
+        'pocket',
+        'mono',
+        'slot'
+    ],
 
-//nHolds
-[
-    'slopers',
-    'jugs',
-    'crimps',
-    'rails',
-    'underclings',
-    'edges',
-    'pinches',
-    'pockets',
-    'monos',
-    'slots'
-]
+    nHolds: [
+        'slopers',
+        'jugs',
+        'crimps',
+        'rails',
+        'underclings',
+        'edges',
+        'pinches',
+        'pockets',
+        'monos',
+        'slots'
+    ],
 
-//nMove
-[
-    'move',
-    'deadpoint',
-    'dyno',
-    'cross',
-    'high-step',
-    'pull',
-    'lock-off',
-    'reach',
-    'drop-knee'
-]
+    nMove: [
+        'move',
+        'deadpoint',
+        'dyno',
+        'cross',
+        'high-step',
+        'pull',
+        'lock-off',
+        'reach',
+        'drop-knee',
+        'mantel'
+    ]
 
-//vStart
-[
-    'establish',
-    'start',
-]
+    vStart: [
+        'establish',
+        'start',
+    ]
 
-//vDo
-[
-    'make',
-    'pull',
-    'do',
-]
+    vDo: [
+        'make',
+        'pull',
+        'do',
+    ],
 
-//vMove
-[
-    'move',
-    'traverse',
-    'thrutch',
-    'huck',
-    'burl',
-    'throw',
-    'pull',
-]
+    vMove: [
+        'move',
+        'traverse',
+        'thrutch',
+        'huck',
+        'burl',
+        'throw',
+        'pull',
+    ],
 
-//vHold
-[
-    'crimp',
-    'squeeze',
-    'pinch',
-    'control',
-    'dominate',
-]
+    vHold: [
+        'crimp',
+        'squeeze',
+        'pinch',
+        'control',
+        'dominate',
+    ],
 
-//climbAdj
-[
-    'thought-provoking',
-    'interesting',
-    'fun',
-    'flowy',
-    'techy',
-    'technical',
-    'mega',
-    'chossy',
-    'insecure',
-    'morpho',
-    'burly',
-    'height-dependent',
-    'sandbagged',
-    'long',
-    'short',
-    'cruxy',
-    'sustained',
-    'deathly',
-    'gym-style',
-    'beta-intensive'
-]
+    climbAdj: [
+        'thought-provoking',
+        'interesting',
+        'fun',
+        'flowy',
+        'techy',
+        'technical',
+        'mega',
+        'chossy',
+        'insecure',
+        'morpho',
+        'burly',
+        'height-dependent',
+        'sandbagged',
+        'long',
+        'short',
+        'cruxy',
+        'sustained',
+        'deathly',
+        'gym-style',
+        'beta-intensive',
+        'spicy'
+    ],
 
-//moveAdj
-[
-    'delicate',
-    'strong',
-    'insecure',
-    'long',
-    'short',
-    'burly',
-    'technical',
-]
+    moveAdj: [
+        'delicate',
+        'strong',
+        'insecure',
+        'long',
+        'short',
+        'burly',
+        'technical',
+        'spicy',
+        'core-intensive',
+        'reachy',
+        'powerful'
+    ],
 
-//holdAdj
-[
-    'slopey',
-    'pinchey',
-    'crimpy',
-    'heinous',
-    'terrible',
-    'barely-there',
-    'credit-card',
-    'sharp',
-    'iron',
-    'glassy',
-    'slick',
-    'sticky',
-    'wide',
-    'deep',
-    'thick',
-    'thin',
-    'wavy',
-    'juggy',
-    'bomber',
-    'sinker',
-    'huge',
-    'giant',
-    'frightful',
-    'full-pad',
-    'half-pad',
-    'quarter-pad',
-    'two-finger',
-    'three-finger'
-]
+    holdAdj: [
+        'slopey',
+        'pinchey',
+        'crimpy',
+        'heinous',
+        'terrible',
+        'barely-there',
+        'credit-card',
+        'sharp',
+        'iron',
+        'glassy',
+        'slick',
+        'sticky',
+        'wide',
+        'deep',
+        'thick',
+        'thin',
+        'wavy',
+        'juggy',
+        'bomber',
+        'sinker',
+        'huge',
+        'giant',
+        'frightful',
+        'full-pad',
+        'half-pad',
+        'quarter-pad',
+        'two-finger',
+        'three-finger'
+    ]
+}
