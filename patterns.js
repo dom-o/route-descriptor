fbf8505d4f5456897c56ce2c2fc2d53ba6827a66
dom-o <m.domonic+github@gmail.com>
2017-12-06 23:39:39 -0500

    Pattern modifications.

--- patterns.js
+++ patterns.js
@@ -1,32 +1,31 @@
+patterns = {
+    //Beginning quantifiers
+    interjections: [
+        'wIntensifier climbAdj',
+        'climbAdj nClimb'
+    ],
 
-//Beginning quantifiers
-[
-    'wIntensifier climbAdj.',
-    'A climbAdj nClimb'
-]
+    //Start descriptions
+    startDesc: [
+        'vStart in nFeature',
+        'vStart on nHold',
+        'vStart on nHold in nFeature'
+    ],
 
-//Start descriptions
-[
-    'vStart in nFeature',
-    'vStart on nHold',
-    'vStart on nHold in nFeature'
-]
+    //Middle descriptions
+    moveDesc: [
+        'vDo a moveAdj nMove',
+        'vMove on nHolds',
+        'vMove to a nHold',
+        'vHold a holdAdj nHold',
+    ],
+    continuations: [
+        'into a holdAdj nHold',
+        'past a holdAdj nHold',
+    ],
 
-//Middle descriptions
-[
-    'vDo a moveAdj nMove',
-    'vMove on nHolds',
-    'vMove to a nHold',
-    'vHold a holdAdj nHold',
-    'Keep it together for a moveAdj nMove',
-    'Stay sharp for a moveAdj nMove'
-]
-[
-    'into a holdAdj nHold',
-    'past a holdAdj nHold',
-]
-
-//End descriptions.
-[
-    ''
-]
+    //End descriptions.
+    finishDesc: [
+        
+    ]
+}
