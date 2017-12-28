1c3a5cbfdf5ff7cf4f59743fb0215e215d2a31ee
dom-o <m.domonic+github@gmail.com>
2017-12-27 22:29:27 -0500

    Basic sentence generator with no punctuation.

--- main.js
+++ main.js
@@ -2,34 +2,73 @@ function randomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-/*generateTemplate() {
-    //beginning
-    //start info
-    //2-5 sentences
-    template = '';
-    
-    
-    
-}*/
+function getRandomWord(type, source) {
+    rand = randomInt(source[type].length);
+    return source[type][rand];
+}
 
+function expandTemplate(template, source) {
+    result = '';
+    template = template.split(' ');
+    console.log(template);
+    for(x=0; x<template.length; x++) {
+        //if we get a probability token
+        if(!isNaN(parseFloat(template[x])))
+        {
+            if(Math.random() >= parseFloat(x)) { //fail the probability test
 
-function generateDescription() {
-    /*template = template.split(' ');
-    for(x in template) {
-        if(words.hasOwnProperty(template[x])) {
-            result += getRandomWord(template[x]);
+                if(template[x+1] == '[') {
+                    while(template[x] != ']') { //skip past all the failed probability tokens
+                        x++; 
+                    }
+                }
+                x++; //skip past closing bracket or single failed prob token
+                //THIS MIGHT BE OFF BY ONE
+            }
+        }
+        else if(template[x] == '[' || template[x] == ']') {
+            //do nothing
+        }
+        else if(source.hasOwnProperty(template[x])) {
+            result += getRandomWord(template[x], source);
         }
         else {
             result += template[x];
         }
         result += ' ';
     }
-    return result;*/
-    return 'aayyy';
+    return result;
+}
+
+function generateTemplate() {
+    //beginning
+    //start info
+    //2-5 sentences
+    template = '';
+    numSentences = randomInt(3);
+    template += patterns.big.startNE[randomInt(patterns.big.startNE.length)];
+    template += ' ';
+    template += patterns.big.startE[randomInt(patterns.big.startE.length)];
+    template += ' ';
+    console.log(template);
+    for(i=1; i<numSentences; i++) {
+        template += patterns.big.midE[randomInt(patterns.big.midE.length)];
+        template += ' ';
+    }
+    
+    console.log(template);
+    return template;
+}
+
+
+function generateDescription(template) {
+    template = expandTemplate(template, patterns.little);
+    template = expandTemplate(template, words);
+    return template;
 }
  
 document.getElementById('descriptor-gen').onclick = function() {
-    document.getElementById('descriptor-space').textContent = generateDescription();
+    document.getElementById('descriptor-space').textContent = generateDescription(generateTemplate());
 };
  
  
