23d4d4c3f52f96947c06b7c7697f8bf4f5bef0aa
dom-o <m.domonic+github@gmail.com>
2017-12-27 22:28:35 -0500

    Reorganize vocab to allow different sentence structures.

--- patterns.js
+++ patterns.js
@@ -1,32 +1,42 @@
 patterns = {
-    //Beginning quantifiers
-    interjections: [
-        'wIntensifier climbAdj',
-        'climbAdj nClimb',
-        'wIntensifier climbAdj nClimb'
-    ],
+    big: {
+        startNE: ['0.5 interjection'],
+        midE: ['moveDesc 0.3 continuation 0.3 [ conjunction 0.3 moveDesc 0.3 continuation ]'],
+        startE: ['startDesc 0.2 [ conjunction moveDesc 0.3 continuation ]']
+    },
+    
+    little: {
+        //Beginning quantifiers
+        interjection: [
+            'wIntensifier climbAdj',
+            '0.2 wIntensifier climbAdj nClimb',
+        ],
 
-    //Start descriptions
-    startDesc: [
-        'vStart in nFeature',
-        'vStart on nHold',
-        'vStart on nHold in nFeature'
-    ],
+        //Start descriptions
+        startDesc: [
+            'vStart 0.4 wPos 0.6 [ on 0.3 holdAdj nHold ] in nFeature',
+        ],
 
-    //Middle descriptions
-    moveDesc: [
-        'vDo a moveAdj nMove',
-        'vMove on nHolds',
-        'vMove to a nHold',
-        'vHold a holdAdj nHold',
-    ],
-    continuations: [
-        'into a holdAdj nHold',
-        'past a holdAdj nHold',
-    ],
+        //Middle descriptions
+        moveDesc: [
+            'vDo a moveAdj nMove',
+            'vMove on nHolds',
+            'vMove to a nHold',
+            'vHold a 0.5 holdAdj nHold',
+        ],
+        continuation: [
+            'into a 0.5 holdAdj nHold',
+            'past a 0.5 holdAdj nHold',
+            'through 0.5 holdAdj nHolds'
+        ],
+        conjunction:[
+            'and',
+            'then'
+        ],
 
-    //End descriptions.
-    finishDesc: [
-        
-    ]
+        //End descriptions.
+        finishDesc: [
+            
+        ]
+    }
 }
