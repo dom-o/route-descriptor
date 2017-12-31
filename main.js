8f001f412a952ac561310244219b64ce2ce99d08
dom-o <m.domonic+github@gmail.com>
2017-12-30 21:50:48 -0500

    Minor refactoring.

--- main.js
+++ main.js
@@ -7,33 +7,53 @@ function getRandomWord(type, source) {
     return source[type][rand];
 }
 
+function addToTemplate(keyword, source) {
+    if(source.hasOwnProperty(keyword)) {
+        return getRandomWord(keyword, source);
+    }
+    else {
+        return keyword;
+    }
+}
+
 function expandTemplate(template, source) {
     result = '';
+    i=0;
     template = template.split(' ');
-    console.log(template);
     for(x=0; x<template.length; x++) {
-        //if we get a probability token
-        if(!isNaN(parseFloat(template[x])))
-        {
-            if(Math.random() >= parseFloat(x)) { //fail the probability test
-
-                if(template[x+1] == '[') {
-                    while(template[x] != ']') { //skip past all the failed probability tokens
-                        x++; 
+        /*if(template[x] == '?') {
+            x++;
+            console.log(template[x]);
+            numOptions = parseInt(template[x]);
+            selected = randomInt(numOptions);
+            while(i<numOptions) {
+                if(i==selected) {
+                    while(template[x] != '[') { x++; }
+                    while(template[x] != ']') {
+                        x++;
+                        result += addToTemplate(template[x], source);
                     }
                 }
-                x++; //skip past closing bracket or single failed prob token
-                //THIS MIGHT BE OFF BY ONE
+                if(template[x] == '[') {i++;}
+            }
+            //while(template[x] != ']') { x++; }
+        }*/
+        if(!isNaN(parseFloat(template[x]))) //if we get a probability token
+        {
+            rand = Math.random();
+            if(rand >= parseFloat(template[x])) { //fail the probability test
+                if(template[x+1] == '[') {
+                    while(template[x] != ']') { //skip past all the failed probability tokens
+                        x++;
+                    }
+                }
             }
         }
         else if(template[x] == '[' || template[x] == ']') {
             //do nothing
         }
-        else if(source.hasOwnProperty(template[x])) {
-            result += getRandomWord(template[x], source);
-        }
         else {
-            result += template[x];
+            result += addToTemplate(template[x], source);
         }
         result += ' ';
     }
@@ -45,15 +65,13 @@ function generateTemplate() {
     //start info
     //2-5 sentences
     template = '';
-    numSentences = randomInt(3);
-    template += patterns.big.startNE[randomInt(patterns.big.startNE.length)];
-    template += ' ';
-    template += patterns.big.startE[randomInt(patterns.big.startE.length)];
-    template += ' ';
-    console.log(template);
-    for(i=1; i<numSentences; i++) {
-        template += patterns.big.midE[randomInt(patterns.big.midE.length)];
-        template += ' ';
+    numSentences = randomInt(1)+1;
+    console.log(numSentences);
+    template += patterns.big.startNE[randomInt(patterns.big.startNE.length)] + ' ';
+    template += patterns.big.startE[randomInt(patterns.big.startE.length)] + ' ';
+    
+    for(i=0; i<numSentences; i++) {
+        template += patterns.big.midE[randomInt(patterns.big.midE.length)] + ' ';
     }
     
     console.log(template);
